fb1a1bb202aa8c12650c3bd1c0b1495a136dc153
Sean Gilligan <sean@msgilligan.com>
2014-10-04 13:59:41 -0700

    Add vagrant user to ‘bitcoin’ group.

--- clone-build-install-bitcoind.sh
+++ clone-build-install-bitcoind.sh
@@ -2,6 +2,7 @@
 REPOURL=$1
 BRANCH=$2
 REPODIR=$3
+USERNAME=`whoami`
 
 # Clone and checkout using passed (usually from Vagrant) parameters
 git clone --no-checkout $REPOURL $REPODIR
@@ -16,6 +17,11 @@ make
 
 # Install as an Upstart service
 sudo ./contrib/msc-ubuntu/install-mastercore-upstart.sh
+
+# Put the vagrant user (usually 'vagrant' or 'ubuntu') in the bitcoin group
+# so that /etc/bitcoin/bitcoin.conf can be seen by bitcoin-cli/mastercore-cli
+sudo usermod -a -G bitcoin $USERNAME
+ 
 
 
 
